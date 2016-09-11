d4b0fa4c0e7095b2ae3343bcf5e47df5e9c20c4b
Wes Davis <wesley.r.davis@gmail.com>
2016-09-11 16:45:39 -0400

    0.7.0 egg

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.daviswr.Cisco.IOS.CPU"
-VERSION = "0.1.0"
+VERSION = "0.7.0"
 AUTHOR = "Wes Davis"
 LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.daviswr', 'ZenPacks.daviswr.Cisco', 'ZenPacks.daviswr.Cisco.IOS']
