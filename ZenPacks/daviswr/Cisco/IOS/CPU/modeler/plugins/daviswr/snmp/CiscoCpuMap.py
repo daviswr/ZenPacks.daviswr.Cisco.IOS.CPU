b91f3c4792667d6b37507d06dd22ed2b1254b852
Wes Davis <wesley.r.davis@gmail.com>
2017-05-01 22:00:51 -0400

    PEP8 compliance

--- ZenPacks/daviswr/Cisco/IOS/CPU/modeler/plugins/daviswr/snmp/CiscoCpuMap.py
+++ ZenPacks/daviswr/Cisco/IOS/CPU/modeler/plugins/daviswr/snmp/CiscoCpuMap.py
@@ -9,6 +9,7 @@ from Products.DataCollector.plugins.CollectorPlugin \
 from Products.DataCollector.plugins.DataMaps \
     import MultiArgs
 
+
 class CiscoCpuMap(SnmpPlugin):
     maptype = 'CPUMap'
     compname = 'hw'
@@ -16,21 +17,29 @@ class CiscoCpuMap(SnmpPlugin):
     modname = 'Products.ZenModel.CPU'
 
     cpmCPUTotalEntry = {
-         '.2': 'cpmCPUTotalPhysicalIndex',
-    }
+        '.2': 'cpmCPUTotalPhysicalIndex',
+        }
 
     entPhysicalEntry = {
-        '.2':'entPhysicalDescr',
-        '.4':'entPhysicalContainedIn',
-        '.7':'entPhysicalName',
-        '.12':'entPhysicalMfgName',
-        '.13':'entPhysicalModelName',
-    }
+        '.2': 'entPhysicalDescr',
+        '.4': 'entPhysicalContainedIn',
+        '.7': 'entPhysicalName',
+        '.12': 'entPhysicalMfgName',
+        '.13': 'entPhysicalModelName',
+        }
 
     snmpGetTableMaps = (
-        GetTableMap('cpmCPUTotalTable', '.1.3.6.1.4.1.9.9.109.1.1.1.1', cpmCPUTotalEntry),
-        GetTableMap('entPhysicalTable', '.1.3.6.1.2.1.47.1.1.1.1', entPhysicalEntry),
-    )
+        GetTableMap(
+            'cpmCPUTotalTable',
+            '.1.3.6.1.4.1.9.9.109.1.1.1.1',
+            cpmCPUTotalEntry
+            ),
+        GetTableMap(
+            'entPhysicalTable',
+            '.1.3.6.1.2.1.47.1.1.1.1',
+            entPhysicalEntry
+            ),
+        )
 
     def condition(self, device, log):
         """determine if this modeler should run"""
@@ -43,59 +52,91 @@ class CiscoCpuMap(SnmpPlugin):
         """collect snmp information from this device"""
         log.info('processing %s for device %s', self.name(), device.id)
         getdata, tabledata = results
+
         cpuTable = tabledata.get('cpmCPUTotalTable')
         if cpuTable is None:
-            log.error('Unable to get data for %s for cpmCPUTotslTable -- skipping model' % device.id)
+            log.error('Unable to get cpmCPUTotalTable for %s' % device.id)
             return None
+        else:
+            log.debug(
+                'cpmCPUTotalTable has % entries',
+                len(cpuTable)
+                )
+
         entTable = tabledata.get('entPhysicalTable')
         if entTable is None:
-            log.error('Unable to get data for %s for entPhysicalTable -- skipping model' % device.id)
+            log.error('Unable to get entPhysicalTable for %s' % device.id)
             return None
+        else:
+            log.debug(
+                'entPhysicalTable has %s entries',
+                len(entTable)
+                )
+
+        # Processors
         rm = self.relMap()
-        #for entity in entTable.items():
-        #    log.debug('Entity %s - %s' % (entity[0], entity[1]))
+
         for snmpindex, row in cpuTable.items():
-            if not rm and not self.checkColumns(row, self.columns, log): 
+            if not rm and not self.checkColumns(row, self.columns, log):
                 return rm
             om = self.objectMap()
             om.snmpindex = snmpindex.strip('.')
 
-            # Many fixed-config switches and small routers return 0
-            # for cpmCPUTotalPhysicalIndex and have no entPhysicalName.0 object,
-            # but have entPhysicalName.1. The Catalyst 3560 is a notable exception,
-            # starting with entPhysicalName.1001.
-            entIndex = str(row['cpmCPUTotalPhysicalIndex'])
-            if not entIndex in entTable:
-                log.debug('Index %s not found in entPhysicalTable, using index %s' % (entIndex, snmpindex))
+            # Many fixed-config switches and small routers return 0 for
+            # cpmCPUTotalPhysicalIndex and have no entPhysicalName.0 object,
+            # but have entPhysicalName.1. The Catalyst 3560 is a notable
+            # exception, starting with entPhysicalName.1001.
+            entIndex = str(row.get('cpmCPUTotalPhysicalIndex', None))
+            if entIndex not in entTable:
+                log.debug(
+                    'Index %s not found in entPhysicalTable, using %s',
+                    entIndex,
+                    snmpindex
+                    )
                 entIndex = str(snmpindex)
-                if not entIndex in entTable:
+                if entIndex not in entTable:
                     entIndex = str(1000 + int(entIndex))
-                    log.debug('Index %s not found in Entity table, using index %s' % (snmpindex, entIndex))
-            model = entTable[entIndex].get('entPhysicalModelName')
+                    log.debug(
+                        'Index %s not found in entPhysicalTable, using %s',
+                        snmpindex,
+                        entIndex
+                        )
+
+            entity = entTable.get(entIndex, dict())
+            model = entity.get('entPhysicalModelName', None)
             if model is None or len(model) == 0:
-                log.debug('entPhysicalModelName not available, using entPhysicalDescr')
-                model = entTable[entIndex].get('entPhysicalDescr')
+                log.debug('entPhysicalModelName not available')
+                model = entity.get('entPhysicalDescr', None)
                 if model is None or len(model) == 0:
-                    log.debug('entPhysicalDescr not available, using entPhysicalName')
-                    model = entTable[entIndex].get('entPhysicalName')
+                    log.debug('entPhysicalDescr not available')
+                    model = entity.get('entPhysicalName', None)
                     if model is None or len(model) == 0:
-                       log.debug('entPhysicalName not available, using default model')
-                       model = 'CPU {}'.format(str(om.snmpindex))
-            if model.lower().find('cpu') < 0 \
-                or model.lower().find('proc') < 0:
+                        log.debug('entPhysicalName not available')
+                        model = 'CPU {}'.format(om.snmpindex)
+
+            if model.lower().find('cpu') < 0 or model.lower().find('proc') < 0:
                 model = 'CPU of {}'.format(model)
+
             mfg = entTable[entIndex].get('entPhysicalMfgName')
             if mfg is None or len(mfg) == 0:
-                log.debug('entPhysicalMfgName not available, using default manufacturer')
+                log.debug('entPhysicalMfgName not available')
                 mfg = 'Cisco'
+
             socket = entTable[entIndex].get('entPhysicalContainedIn')
             if socket is None or len(str(socket)) == 0:
-              log.debug('entPhysicalContainedIn not available, using 0 for socket')
-              socket = '0'
-              # TODO: Set socket value in ObjectMap
-            log.debug('Index: %s, Model: %s, Manufacturer: %s' % (entIndex, model, mfg))
+                log.debug('entPhysicalContainedIn not available, using 0')
+                socket = '0'
+                # TODO: Set socket value in ObjectMap
+
+            log.debug(
+                'Index: %s, Model: %s, Manufacturer: %s',
+                entIndex,
+                model,
+                mfg
+                )
             om.setProductKey = MultiArgs(model, mfg)
             om.id = self.prepId(model)
             rm.append(om)
 
+        log.debug('%s RelMap:\n%s', self.name(), str(rm))
         return rm
